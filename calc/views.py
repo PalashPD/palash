8f8d3d1ade1781b437185fa6fcd658eea3cf7640
Palash Pratim Deka <64046049+PalashPD@users.noreply.github.com>
2020-07-10 15:41:42 +0530

    Update views.py

--- calc/views.py
+++ calc/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 # Create your views here.
 def home(request):
-    return render(request,'index.html',{'title':'kkk'})
+    return render(request,'index.html')
 def naam(request):
     k=request.GET['buddy']
     return render(request,'abc.html',{'title':k})
